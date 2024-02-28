1db738d9ed9ae186e2b789e134ff3b31c1538a9b
seand23 <b00156175@mytudublin.ie>
2024-02-28 21:49:10 +0000

    .

--- project/store.php
+++ project/store.php
@@ -1,15 +1,21 @@
 <?php
+require 'lib/functions.php';
+$suits = get_suits();
+$suits = array_reverse($suits);
+var_dump($suits);
+?>
+
+<?php
 require 'layout/header.php';
 ?>
     <main>
         <div class="container">
-            <section class="featured-items">
                 <h2>Featured Suits</h2>
-                <div class="featured-suits">
-                    <img src="images/stock.jpg" alt="Featured suit 1">
-                    <img src="images/stock.jpg" alt="Featured suit 2">
-                </div>
-            </section>
+                <?php foreach ($suits as $testsuit) { ?>
+              <blockquote class="test">
+                <h2><?php echo $testsuit['name']?></h2>
+              </blockquote>
+              <?php }?>
         </div>
     </main>
 

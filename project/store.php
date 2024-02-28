2f1b3f57f925b2bd683b71f78cc02e0171fe6f62
seand23 <145985702+seand23@users.noreply.github.com>
2024-02-28 22:30:17 +0000

    reading from db - yay

--- project/store.php
+++ project/store.php
@@ -11,13 +11,14 @@ require 'layout/header.php';
         <div class="container">
                 <h2>Featured Suits</h2>
                 <?php foreach ($suits as $testsuit) { ?>
+              <img src="images/<?php echo $testsuit['image'];?>" class="image"/>
               <blockquote class="test">
                 <h2><?php echo $testsuit['id']?></h2>
                 <h2><?php echo $testsuit['suit_name']?></h2>
                 <h2><?php echo $testsuit['brand']?></h2>
                 <h2><?php echo $testsuit['price']?></h2>
+                <h2><?php echo $testsuit['image']?></h2>
               </blockquote>
-              <img src="images/<?php echo $testsuit['image'];?>" class="image"/>
               <?php }?>
         </div>
     </main>

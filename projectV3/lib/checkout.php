066945bcc775b76ae9cef24638f8355930364dae
seand23 <145985702+seand23@users.noreply.github.com>
2024-03-16 20:32:23 +0000

    Create checkout.php

---
+++ projectV3/lib/checkout.php
@@ -0,0 +1,9 @@
+<?php
+class Checkout{
+    function sendOrderToDB(){
+        //take order in as an object
+        //prepare sql statement "INSERT INTO ...."
+        //write order to db
+        //give user confirmation
+    }
+}

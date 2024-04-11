46c22efd5dfa3b9ff9f715f4fd318684e14722ba
seand23 <145985702+seand23@users.noreply.github.com>
2024-04-11 20:44:11 +0100

    added 5.4 back , got removed somehow

---
+++ projectV5.4/public/layout/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blanch Suits</title>
+    <link rel="stylesheet" href="./style/main.css">
+    <link rel="stylesheet" href="./style/nav.css">
+    <link rel="stylesheet" href="./style/slider.css">
+    <link rel="stylesheet" href="./style/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+</head>
+       
+<nav>
+    <div class="topnav" id="myTopnav">
+  <div class="logo"><a href="index.php"><img src="./images/LOGO.jpg" style="width:50%"></a> </div>
+  <div class="menu">
+    <a href="index.php">Home</a>
+    <a href="store.php">Store</a>
+    <a href="contact.php">Contact</a>
+  </div>
+  <div class="search-bar">
+    <input type="text" placeholder="Search...">
+    <button type="search">Search</button>
+  </div>
+  <div class="login">
+    <button><a href="login-register.php">Login/Register</a></button>
+    <button><a href="ship.php">Cart</a></button>
+  </div>
+  <a href="javascript:void(0);" class="icon" onclick="burgerNav()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+</nav>
+

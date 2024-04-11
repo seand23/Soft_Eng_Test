bb9c06656f7df100631234d5e94d75a767645bcd
AEnnis04 <b00152710@mytudublin.ie>
2024-04-11 20:40:31 +0100

    updated home page and other stuff

--- projectV5.5 -- USE ME/lib/config/config.php
+++ projectV5.5 -- USE ME/lib/config/config.php
@@ -3,5 +3,5 @@
 return array(
     'database_dsn' => 'mysql:dbname=softeng;host=localhost:3306', //Reminder for mac to use the :3306 localhost
     'database_user' => 'root',
-    'database_pass' => '' //Reminder to put in your db password
+    'database_pass' => 'Ireland19?!@' //Reminder to put in your db password
 );
--- projectV5.5 -- USE ME/public/js/slider.js
+++ projectV5.5 -- USE ME/public/js/slider.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         slideIndex++;
         if (slideIndex > slides.length) {slideIndex = 1}   
         slides[slideIndex-1].style.display = "block";  
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        setTimeout(showSlides, 6000); // Change image every 2 seconds
     }
 });
 
@@ -29,14 +29,10 @@ var slideIndex = 1;
     function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("mySlide");
-        var dots = document.getElementsByClassName("dot");
         if (n > slides.length) {slideIndex = 1}
         if (n < 1) {slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
         }
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";

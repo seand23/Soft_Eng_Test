834fe319ffa5782a8d39c1e6bda4902b1a5bd5bb
AEnnis04 <b00152710@mytudublin.ie>
2024-04-17 18:16:13 +0100

    updated doc and website

--- projectV5.7/public/confirmation.php
+++ projectV5.7/public/confirmation.php
@@ -4,7 +4,11 @@ $order = new Order();
 ?>
 <?php require_once 'layout/header.php'; ?>
 <main>
+    <div class="confP">
     <h1>Thank you</h1>
     <h2>Your Order has been confirmed</h2>
+    </div>
+    <div class="Cost">
     <?php $order->displayCartItems() ?>
+    </div>
     <?php require 'layout/footer.php'; ?>

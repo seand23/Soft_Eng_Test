71538892f791e6f470ce7920367a6f56c83b0438
seand23 <145985702+seand23@users.noreply.github.com>
2024-04-28 21:15:37 +0100

    .

---
+++ projectV9/lib/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'database_dsn' => 'mysql:dbname=softeng;host=localhost:3306', //Reminder for mac to use the :3306 localhost
+    'database_user' => 'root',
+    'database_pass' => 'Gfke4942' //Reminder to put in your db password
+);

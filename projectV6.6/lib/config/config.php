c040138568c4de7f36dd7768f6c6b99199668ea3
seand23 <145985702+seand23@users.noreply.github.com>
2024-04-28 15:18:47 +0100

    added unit test for cart display and added unit test for add user , unit testing should be done idfk

---
+++ projectV6.6/lib/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'database_dsn' => 'mysql:dbname=softeng;host=localhost:3306', //Reminder for mac to use the :3306 localhost
+    'database_user' => 'root',
+    'database_pass' => 'Kvjn2762!' //Reminder to put in your db password
+);

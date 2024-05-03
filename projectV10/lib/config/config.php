2b2d97675cdeef48228d520a2ee54c6b9dc60dd9
seand23 <145985702+seand23@users.noreply.github.com>
2024-05-03 14:43:59 +0100

    UPLOAD ITBM

--- projectV10/lib/config/config.php
+++ projectV10/lib/config/config.php
@@ -5,7 +5,7 @@
  */
 $host = "localhost";
 $username = "root";
-$password = "Ireland19?!@";
+$password = "";
 $dbname = "softeng"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
